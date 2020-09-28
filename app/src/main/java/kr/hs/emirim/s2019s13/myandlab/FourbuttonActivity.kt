a5d85a429eb768ad03618e3811c660bef45abbfa
Shinipsae <s2019s13@e-mirim.hs.kr>
2020-09-28 20:04:41 +0900

    create buttons using Kotlin

--- app/src/main/java/kr/hs/emirim/s2019s13/myandlab/FourbuttonActivity.kt
+++ app/src/main/java/kr/hs/emirim/s2019s13/myandlab/FourbuttonActivity.kt
@@ -1,11 +1,59 @@
 package kr.hs.emirim.s2019s13.myandlab
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class FourbuttonActivity : AppCompatActivity() {
+class FourbuttonActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mButton1 : Button
+    private lateinit var mButton2 : Button
+    private lateinit var mButton3 : Button
+    private lateinit var mButton4 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourbutton)
+
+        mButton1 = findViewById(R.id.button1)
+        mButton2 = findViewById(R.id.button2)
+        mButton3 = findViewById(R.id.button3)
+        mButton4 = findViewById(R.id.button4)
+
+        mButton1.setOnClickListener(this)
+        mButton2.setOnClickListener(this)
+        mButton3.setOnClickListener(this)
+        mButton4.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.button1 -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(""))
+                startActivity{this}
+            }
+            R.id.button1 -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(""))
+                startActivity{this}
+            }
+            R.id.button1 -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(""))
+                startActivity{this}
+            }
+            
+
+
+            )
+            R.id.button2 -> {}
+            R.id.button3 -> {}
+            R.id.button4 -> finish()
+
+
+        }
+
     }
 }
